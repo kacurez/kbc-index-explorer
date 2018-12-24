648073681af2ca73002a36f790ed9cdef711b3fd
Tomas Kacur <tomas.kacur@keboola.com>
2018-12-24 16:02:34 +0100

    load index and render it as text

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,11 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
+import Http
+
+
+
+-- MAIN
 
 
 main : Program () Model Msg
@@ -14,29 +19,63 @@ main =
         }
 
 
-type alias Model =
+
+-- MODEL
+
+
+type alias Data =
     String
 
 
-type Msg
-    = NoOp
+type Model
+    = Loading
+    | Success Data
+    | Failure String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "Tomas", Cmd.none )
+    ( Loading
+    , Http.get { url = "https://connection.keboola.com/v2/storage", expect = Http.expectString GotData }
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | GotData (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        GotData result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
+
+                Err _ ->
+                    ( Failure "there was a problem", Cmd.none )
+
         NoOp ->
             ( model, Cmd.none )
 
 
+
+-- VIEW
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "Hello "
-        , text model
-        ]
+    case model of
+        Loading ->
+            div [] [ text "Loading" ]
+
+        Success fullText ->
+            div [] [ text fullText ]
+
+        Failure _ ->
+            div [] [ text "there was an error" ]
