55fb2bfd9f78b5b2a1216764d9f910bf067fdcf0
Tomas Kacur <tomas.kacur@keboola.com>
2018-12-24 17:06:56 +0100

    load kbc index and render it simply

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,23 @@
 module Main exposing (main)
 
 import Browser
+import Dict exposing (Dict)
 import Html exposing (..)
 import Http
+import Json.Decode as Decode
+    exposing
+        ( Decoder
+        , bool
+        , dict
+        , float
+        , int
+        , lazy
+        , list
+        , map
+        , null
+        , oneOf
+        , string
+        )
 
 
 
@@ -23,20 +38,26 @@ main =
 -- MODEL
 
 
-type alias Data =
-    String
+type JsonValue
+    = JsonString String
+    | JsonInt Int
+    | JsonFloat Float
+    | JsonBoolean Bool
+    | JsonArray (List JsonValue)
+    | JsonObject (Dict String JsonValue)
+    | JsonNull
 
 
 type Model
     = Loading
-    | Success Data
+    | Success JsonValue
     | Failure String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( Loading
-    , Http.get { url = "https://connection.keboola.com/v2/storage", expect = Http.expectString GotData }
+    , loadData
     )
 
 
@@ -46,7 +67,7 @@ init _ =
 
 type Msg
     = NoOp
-    | GotData (Result Http.Error String)
+    | GotData (Result Http.Error JsonValue)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -74,8 +95,58 @@ view model =
         Loading ->
             div [] [ text "Loading" ]
 
-        Success fullText ->
-            div [] [ text fullText ]
+        Success json ->
+            viewJson json
 
         Failure _ ->
             div [] [ text "there was an error" ]
+
+
+viewJson : JsonValue -> Html Msg
+viewJson json =
+    case json of
+        JsonString value ->
+            div [] [ text ("\"" ++ value ++ "\",\n") ]
+
+        JsonInt value ->
+            div [] [ text (String.fromInt value ++ ",") ]
+
+        JsonFloat value ->
+            div [] [ text (String.fromFloat value ++ ",") ]
+
+        JsonBoolean value ->
+            div [] [ text "boolean" ]
+
+        JsonArray value ->
+            div [] [ text "[", div [] (List.map viewJson value), text "]" ]
+
+        JsonObject value ->
+            div [] (List.map (\( k, v ) -> div [] [ text ("\"" ++ k ++ "\""), viewJson v ]) (Dict.toList value))
+
+        JsonNull ->
+            div [] [ text "null" ]
+
+
+
+-- HTTP
+
+
+loadData : Cmd Msg
+loadData =
+    Http.get
+        { url = "https://connection.keboola.com/v2/storage"
+        , expect = Http.expectJson GotData decoder
+        }
+
+
+decoder : Decoder JsonValue
+decoder =
+    oneOf
+        [ map JsonString string
+        , map JsonInt int
+        , map JsonFloat float
+        , map JsonBoolean bool
+        , list (lazy (\_ -> decoder)) |> map JsonArray
+        , dict (lazy (\_ -> decoder)) |> map JsonObject
+        , null JsonNull
+        ]
